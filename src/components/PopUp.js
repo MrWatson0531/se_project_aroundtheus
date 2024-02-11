31f3a0e8728fc2d48a738879013807a4f2b2c9a2
MrWatson0531 <135671943+MrWatson0531@users.noreply.github.com>
2024-02-11 11:15:33 -0700

    Update PopUp.js

--- src/components/PopUp.js
+++ src/components/PopUp.js
@@ -1,27 +1,20 @@
 export default class Popup {
   constructor({ popupSelector }) {
     this._popupElement = document.querySelector(popupSelector);
-    console.log(this._popupElement);
     this._button = this._popupElement.querySelector(".modal__close");
   }
 
   open() {
-    // open popup
-    console.log(this);
     this._popupElement.classList.add("modal_opened");
     document.addEventListener("keydown", this._handleEscape);
   }
 
   close() {
-    //close popup
-
     document.removeEventListener("keydown", this._handleEscape);
     this._popupElement.classList.remove("modal_opened");
   }
 
   _handleEscape = (e) => {
-    console.log(this);
-    //listen for esc button
     if (e.key === "Escape") {
       this.close();
     }
